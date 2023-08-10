8277556c30f3e8e79892a7b24db16c44d9ae67ad
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-08-10 12:57:03 -0500

    Add methods in Rol, Menu, Categoria, Producto services

--- src/app/Services/producto.service.ts
+++ src/app/Services/producto.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ResponseApi } from '../Interfaces/response-api';
+
+import { Producto } from '../Interfaces/producto';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
 
-  constructor() { }
+  private urlApi:string = environment.endpoint + "Producto/";
+
+  constructor(private http:HttpClient) { }
+
+  
+
+  list():Observable<ResponseApi>{
+    return this.http.get<ResponseApi>(`${this.urlApi}Lista`)
+  }
+
+
+  guardar(request: Producto):Observable<ResponseApi>{
+    return this.http.post<ResponseApi>(`${this.urlApi}Guardar`, request)
+  }
+
+
+  editar(request: Producto):Observable<ResponseApi>{
+    return this.http.put<ResponseApi>(`${this.urlApi}Editar`, request)
+  }
+
+
+  elininar(id:number):Observable<ResponseApi>{
+    return this.http.delete<ResponseApi>(`${this.urlApi}Eliminar/${id}`)
+  }
 }
--- src/app/Services/usuario.service.ts
+++ src/app/Services/usuario.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { ResponseApi } from '../Interfaces/response-api';
+
 import { Login } from '../Interfaces/login';
 import { Usuario } from '../Interfaces/usuario';
 
@@ -12,8 +13,7 @@ import { Usuario } from '../Interfaces/usuario';
 })
 export class UsuarioService {
 
-  private urlApi:string = environment.endpoint + "Usuario/"
-
+  private urlApi:string = environment.endpoint + "Usuario/";
 
   constructor(private http:HttpClient) { }
 
