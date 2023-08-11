ba36074ce2dde5d4b766ac8cf04b23836dc7be7a
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-08-10 23:51:21 -0500

    Add Layout, Utilidad service and Login logic

---
+++ src/app/Reutilizable/utilidad.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+
+import { MatSnackBar } from '@angular/material/snack-bar'; 
+import { Sesion } from '../Interfaces/sesion';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilidadService {
+
+  constructor(private _snackBar:MatSnackBar) { }
+
+
+
+  mostrarAlerta(mensaje:string, tipo:string){
+
+    this._snackBar.open(mensaje,tipo,{
+      horizontalPosition:"end",
+      verticalPosition:"top",
+      duration:3000
+    })
+
+  }
+
+
+  guardarSesionUsuario(usuarioSesion:Sesion){
+
+    //permite guardar información en memoria en el navegador del usuario
+    localStorage.setItem("usuario", JSON.stringify(usuarioSesion));
+  }
+
+
+  obtenerSesionUsuario(usuarioSesion:Sesion){
+
+    const dataCadena = localStorage.getItem("usuario");
+
+    const usuario = JSON.parse(dataCadena!);
+    return usuario;
+  }
+
+
+  eliminarSesionUsuario(){
+    localStorage.removeItem("usuario");
+  }
+
+
+}
